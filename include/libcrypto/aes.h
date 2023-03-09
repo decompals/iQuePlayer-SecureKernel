2fb8bcdd960062cb8da447f67aca0d3814f44ab7
pixel-stuck <mathmcclintic@gmail.com>
2023-03-08 23:39:50 -0500

    fix aes return type

--- include/libcrypto/aes.h
+++ include/libcrypto/aes.h
@@ -1,8 +1,8 @@
 #ifndef AES_H
 #define AES_H
 
-aes_HwKeyExpand(u32* key, u32* expandedKey);
-aes_SwEncrypt(u32* key, u32* iv, void* in, u32 size, void* out);
-aes_SwDecrypt(u32* key, u32* iv, void* in, u32 size, void* out);
+void aes_HwKeyExpand(u32* key, u32* expandedKey);
+void aes_SwEncrypt(u32* key, u32* iv, void* in, u32 size, void* out);
+void aes_SwDecrypt(u32* key, u32* iv, void* in, u32 size, void* out);
 
 #endif
